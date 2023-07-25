bddc4396887ee5974bd6b6540b335232a0ddce94
Rui Bastos <rui.bastos@springernature.com>
2023-07-25 10:07:07 +0100

    day08

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,24 @@
+from datetime import time, datetime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-st.header("st.write")
+st.header("st.slider")
 
-st.write("Hello, *World!* :sunglasses:")
+st.subheader("Slider")
+age = st.slider("How old are you?", 0, 130, 25)
+st.write("I'm ", age, "years old"")
 
-st.write(1234)
+st.subheader("Range slider")
 
-df = pd.DataFrame({"col01": [1, 2, 3, 4], "col02": [10, 20, 30, 40]})
-st.write(df)
+values = st.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
+st.write("Values:", values)
 
-st.write("Below is a DataFrame:", df, "Above is a DataFrame.")
+st.subheader("Range time slider")
+appointment = st.slider("Schedule your appointment:", value=(time(11, 30), time(12, 45)))
+st.write("You're schedule for:", appointment)
 
-fig = plt.figure()
-df2 = pd.DataFrame(np.random.randn(200, 3), columns=["a", "b", "c"])
-c = plt.scatter(df2["a"], df2["b"], c=df2["c"], figure=fig)
-st.write(fig)
+st.subheader("Datetime slider")
+start_time = st.slider("When do you start?", value=datetime(2020, 1, 1, 9, 30), format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
